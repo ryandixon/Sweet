52335e9edfa548951cd592645c9f81cf7f0bca20
Ryan Dixon <dixon104@marshall.edu>
2016-10-07 04:29:06 -0400

    Commit 1.

---
+++ Sweet/Controllers/TicketsController.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Sweet.Models;
+
+namespace Sweet.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public TicketsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            var tickets = _dbContext.Tickets.ToList();
+            return View(tickets);
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Add(Ticket ticket)
+        {
+            _dbContext.Tickets.Add(ticket);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            var ticket = _dbContext.Tickets.SingleOrDefault(v => v.Id == id);
+
+            if (ticket == null)
+                return HttpNotFound();
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Ticket ticket)
+        {
+            var ticketInDb = _dbContext.Tickets.SingleOrDefault(v => v.Id == ticket.Id);
+            if (ticketInDb == null)
+                return HttpNotFound();
+
+            ticketInDb.Title = ticket.Title;
+            ticketInDb.Description = ticket.Description;
+            ticketInDb.Urgency = ticket.Urgency;
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var ticket = _dbContext.Tickets.SingleOrDefault(v => v.Id == id);
+
+            if (ticket == null)
+                return HttpNotFound();
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public ActionResult DoDelete(int id)
+        {
+            var ticket = _dbContext.Tickets.SingleOrDefault(v => v.Id == id);
+
+            if (ticket == null)
+                return HttpNotFound();
+
+            _dbContext.Tickets.Remove(ticket);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Sweet/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sweet.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public String Description { get; set; }
+        public Urgency Urgency { get; set; }
+    }
+    public enum Urgency
+    {
+        High = 1,
+        Med,
+        Low
+    }
+}
