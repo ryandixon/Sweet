675b41332e6754d3e593c0cd97707a59edb602d7
Ryan Dixon <dixon104@marshall.edu>
2016-10-10 03:21:26 -0400

    Commit 6

---
+++ Sweet/Controllers/CalenderController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.Mvc;
+
+
+namespace Sweet.Controllers
+{
+    public class CalenderController : Controller
+    {
+        // GET: Calender
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
